2b748262bd5b60a77271f37c4b9e4bbf471780ac
Dixiepixie159 <dva5606@stacmail.net>
2024-03-06 14:00:16 +1300

    Svelte Testing v1
    Created a svelte app purely for testing purposes. Managed to add a page and create a script which introduces a counter through javascript.

---
+++ JsApps/my-app/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>bing bong</p>
+<a href="/about">Very cool link</a>
+
+<script>
+	let name = 'bebebebbe';
+</script>
+
+<h2>bing bing {name.toUpperCase()} dong dong</h2>
