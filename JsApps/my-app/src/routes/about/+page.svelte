bd23035b6682084d14d3708b0bfab2100e4301fc
Dixiepixie159 <dva5606@stacmail.net>
2024-03-20 17:33:58 +1300

    Svelte Testing v3
    Added in the css styles sheet for FlowBite allowing me to easily use it. Had to change a few file names to add the new css file. Deleted my previous styles in +layout.svelte and added them in as a class in the main +page.svelte file. Dark mode is also somewhat working as I can change elements by clicking the dark mode button which is essentially what it's supposed to do.

--- JsApps/my-app/src/routes/about/+page.svelte
+++ JsApps/my-app/src/routes/about/+page.svelte
@@ -4,23 +4,34 @@
     import { FireOutline } from 'flowbite-svelte-icons';
 </script>
 
-<h1 class="dark:text-gray-400 dark:bg-blue-900 ">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<p>chickenchickenchicken</p>
+<svelte:head>
+  <link rel="stylesheet" href="">
+</svelte:head>
 
-<Counter />
-
+<body class="bg-white-900 dark:bg-gray-900">
   
-  <div class="p-8">
-    <Alert>
-      <span class="font-medium">Info alert!</span>
-      Change a few things up and try submitting again.
-    </Alert>
-  </div>
 
-  <Toast color="primary">
-    <svelte:fragment slot="icon">
-      <FireOutline class="w-6 h-6" />
-    </svelte:fragment>
-    Dismissable user notification.
-  </Toast>
+  <h1 class="dark:text-gray-400 dark:bg-blue-900 ">Welcome to SvelteKit</h1>
+  <p class="dark:text-gray-400">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+  <p>chickenchickenchicken</p>
+
+  <Counter />
+
+    
+    <div class="p-8">
+      <Alert>
+        <span class="font-medium">Info alert!</span>
+        Change a few things up and try submitting again.
+      </Alert>
+    </div>
+
+    <Toast color="primary">
+      <svelte:fragment slot="icon">
+        <FireOutline class="w-6 h-6" />
+      </svelte:fragment>
+      Dismissable user notification.
+    </Toast>
+</body>
+
+<style>
+</style>
