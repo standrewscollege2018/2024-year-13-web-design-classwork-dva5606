bb203bbd4c8f443636414798516e77811fd1e6c1
Dixiepixie159 <dva5606@stacmail.net>
2024-04-16 16:48:19 +1200

    Svelte Testing v5
    I have made a few major changes to my code as well as fixed all of my bugs generated while I made these changes. I have not updated in a while due to multiple issues I was facing at that time. I added in a streaming button that uses the devices camera through html and js. I then played around with html inputs and see if I could upload/take pictures to my app and it worked. My phone did not work because google chrome and android 14 cause a couple issues when it came to cameras. I spent a while solving this bug until I found a relatively recent forum page that said it was a bug. I also made a script that set a variable to an uploaded image's dataurl and so my testing for now if complete

--- JsApps/my-app/capacitor.config.ts
+++ JsApps/my-app/capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'my-app',
-  webDir: 'build',
+  webDir: 'www',
   bundledWebRuntime: false
 };
 
--- JsApps/my-app/src/routes/about/+page.svelte
+++ JsApps/my-app/src/routes/about/+page.svelte
@@ -1,60 +1,106 @@
 <Counter />
-
 <script>
-    import Counter from "../components/counter.svelte";
-    import { Button, Alert, Card, Navbar, NavBrand, NavLi, NavUl, NavHamburger, Toast } from 'flowbite-svelte';
-    import { FireOutline } from 'flowbite-svelte-icons';
-    let stream;
-    let videoRef;
-
-    async function getStream() {
-        try {
-            stream = await navigator.mediaDevices.getUserMedia({
-                video: true,
-                audio: false
-            });
-            videoRef.srcObject = stream;
-        } catch (err) {
-            console.error(err);
-        }
-        console.log(stream.getTracks()[0])
+  import Counter from "../components/counter.svelte";
+  import { Button, Alert, Card, Navbar, NavBrand, NavLi, NavUl, NavHamburger, Toast } from 'flowbite-svelte';
+  import { FireOutline } from 'flowbite-svelte-icons';
+  import { Geolocation } from '@capacitor/geolocation';
+  import { Camera, CameraResultType } from '@capacitor/camera';
+  import { onMount } from 'svelte';
+  
+  
+  console.log("test")
+  let loc = null;
+    async function getCurrentPosition(){
+      const res = await Geolocation.getCurrentPosition()
+      loc = res
+      console.log("frog")
+    
     }
+  let stream;
+  let videoRef;
 
-    async function stopStream() {
-        stream.getTracks().forEach(track => track.stop());
-        videoRef.srcObject = null;
-    }
+  async function getStream() {
+
+    
+      try {
+          stream = await navigator.mediaDevices.getUserMedia({
+              video: true,
+              audio: false
+          });
+          videoRef.srcObject = stream;
+      } catch (err) {
+          console.error(err);
+      }
+      console.log(stream.getTracks()[0])
+  }
+
+  async function stopStream() {
+      stream.getTracks().forEach(track => track.stop());
+      videoRef.srcObject = null;
+  }
+
+  onMount(() => {
+
+    const fileInput = document.querySelector('input[type="file"]');
+
+    fileInput.addEventListener('change', () => {
+    console.log(fileInput.files)
+    
+    const fr = new FileReader();
+
+    fr.readAsDataURL(fileInput.files[0]);
+
+    fr.addEventListener('load', () => {
+      const url = fr.result;
+      console.log(url);
+    
+    })
+    })})
+  
+  
+  
+
+
 </script>
+
 
 <svelte:head>
   <link rel="stylesheet" href="">
+  
 </svelte:head>
 
 <body class="bg-white-900 dark:bg-gray-900">
+
+<h1 class="dark:text-gray-400">Welcome to SvelteKit</h1>
+<p class="dark:text-gray-400">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div>
+  <h1>Geolocation</h1>
+  <p>Your location is:</p>
+  <p>Latitude: {loc?.coords.latitude}</p>
+  <p>Longitude: {loc?.coords.longitude}</p>
+
+  <button on:click={getCurrentPosition}>
+    Get Current Location
+  </button>
+</div>
+
   
+  <div class="p-8">
+    <Alert>
+      <span class="font-medium">Info alert!</span>
+      Change a few things up and try submitting again.
+    </Alert>
+  </div>
 
-  <h1 class="dark:text-gray-400 dark:bg-blue-900 ">Welcome to SvelteKit</h1>
-  <p class="dark:text-gray-400">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-  <p>chickenchickenchicken</p>
+  <Toast color="primary">
+    <svelte:fragment slot="icon">
+      <FireOutline class="w-6 h-6" />
+    </svelte:fragment>
+    Dismissable user notification.
+  </Toast>
 
-
-
-    
-    <div class="p-8">
-      <Alert>
-        <span class="font-medium">Info alert!</span>
-        Change a few things up and try submitting again.
-      </Alert>
-    </div>
-
-    <Toast color="primary">
-      <svelte:fragment slot="icon">
-        <FireOutline class="w-6 h-6" />
-      </svelte:fragment>
-      Dismissable user notification.
-    </Toast>
-
-    <section class="container mx-auto px-4">
+  <section class="container mx-auto px-4">
     <h1 class="text-4xl text-blue-500 my-4">Webcam Stream Mastery</h1>
     <button class="rounded-sm bg-slate-600 text-white px-4 py-2" on:click={getStream}>Start Stream</button>
     <button class="rounded-sm bg-red-600 text-white px-4 py-2" on:click={stopStream}>Stop Stream</button>
@@ -62,5 +108,14 @@
     <video class="mt-4 rounded-sm " width="640" height="480" autoplay={true} bind:this={videoRef} />
 
 </section>
+
+
+
+<label for="picture">camera</label>
+<input type="file" id="picture" name="picture" accept="image/*" capture="environment" />
+
+<label for="picture">maybemaybe</label>
+<input type="file" id="picture" name="picture" accept="image/*" capture="environment" />
+
 </body>
 
